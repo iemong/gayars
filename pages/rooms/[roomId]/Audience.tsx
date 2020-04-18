9b0e0a5a04f4609cb8770ee8e5b796428db6e773
iemong <kiyoshidainagon@gmail.com>
2020-04-18 15:06:30 +0900

    routingを追加

---
+++ pages/rooms/[roomId]/Audience.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import styled from '@emotion/styled'
+
+export default () => {
+    return <Wrapper>参加者画面</Wrapper>
+}
+
+const Wrapper = styled.div``
---
+++ pages/rooms/[roomId]/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import styled from '@emotion/styled'
+
+export default () => {
+    return <Wrapper>会場を作成しました</Wrapper>
+}
+
+const Wrapper = styled.div``
