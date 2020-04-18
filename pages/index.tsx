f91a93ea9243a529d3da4a448d3475757cfb509b
iemong <kiyoshidainagon@gmail.com>
2020-04-18 14:59:49 +0900

    buttonをとりあえず用意する

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import styled from '@emotion/styled'
 import Head from '../src/components/organisms/Head'
 import Header from '../src/components/organisms/Header'
+import Button from '../src/components/atoms/Button'
 
 const Home = () => {
     return (
@@ -9,6 +10,10 @@ const Home = () => {
             <Head title={'gayars'} />
             <Header />
             <Title>Hello, world!</Title>
+            <Button>会場を作る</Button>
+            <Button styleType="invert">会場を作る</Button>
+            <Button styleType="circle">ON</Button>
+            <Button styleType="square">笑い</Button>
         </>
     )
 }
