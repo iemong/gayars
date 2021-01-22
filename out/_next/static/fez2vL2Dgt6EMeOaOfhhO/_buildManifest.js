1f17293d2fcfa965b32b2edce7ec133800154020
iemong <kiyoshidainagon@gmail.com>
2021-01-22 21:25:49 +0900

    buildファイルをup

--- out/_next/static/fez2vL2Dgt6EMeOaOfhhO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/rooms/[roomId]":[a,b]}}("static\u002Fchunks\u002F8673774c.097656ace741470b33d7.js","static\u002Fchunks\u002Fb210cfdd6aab5c48d7f3fdcc606ab975c58333d6.17702fe3f5a56ee95503.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
