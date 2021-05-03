7ed014a8ccf1e7d2ce2f531552beef884b9380d6
iemong <kiyoshidainagon@gmail.com>
2021-05-03 16:42:12 +0900

    storybookは使わない

--- .storybook/middleware.js
+++
@@ -1,6 +0,0 @@
-// middleware.js
-
-const express = require('express');
-const path = require('path');
-
-module.exports = router => router.use('/', express.static(path.join(__dirname, '../public')));
